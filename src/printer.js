c4208b4e13b59f41560feba11078e1b9b3a855ce
Javier Diaz <javierdiazt406@gmail.com>
2025-01-31 14:16:12 -0400

    Feat: QR and Image support

--- src/printer.js
+++ src/printer.js
@@ -1,19 +1,94 @@
 const escpos = require('escpos');
+var im = require('imagemagick');
+const path = require('path');
+
+const { writeFileSync } = require('fs');
+
 escpos.USB = require('escpos-usb');
 
-function printText(text) {
-  try {
-    const device = new escpos.USB();
-    const printer = new escpos.Printer(device);
+const device = new escpos.USB();
+const printer = new escpos.Printer(device);
 
-    device.open(() => {
-      printer
-        .text(text)
-        .cut()
-        .close();
-    })
+async function printText(input) {
+  try {
+    device.open(async function () {
+      printer.size(0.5, 0.5);
+
+      const lines = input.split("\n");
+
+      for (const line of lines) {
+        const trimmedLine = line.trim();
+        if (!trimmedLine) continue;
+
+        const match = trimmedLine.match(/\[(.*?)\](.*?)\[\/(.*?)\]/);
+
+        if (match) {
+          const [, tag, content] = match;
+
+          switch (tag.toLowerCase()) {
+            case "center":
+              printer.align("ct").text(content);
+              break;
+            case "left":
+              printer.align("lt").text(content);
+              break;
+            case "right":
+              printer.align("rt").text(content);
+              break;
+            case "bold":
+              printer.style("B").text(content);
+              break;
+            case "underline":
+              printer.style("U").text(content);
+              break;
+            case "big":
+              printer.size(1, 1).text(content);
+              break;
+            case "qr":
+              await new Promise(resolve => {
+                printer.align('ct').qrimage(content, { type: 'png', size: 10 }, resolve);
+              });
+              printer.feed(2);
+              break;
+            case "img":
+              await printBase64Image(content, printer);
+              break;
+          }
+        } else if (trimmedLine.toUpperCase() === "[LINE]") {
+          printer.drawLine();
+        } else if (trimmedLine.toUpperCase() === "[CUT]") {
+          printer.feed().cut();
+        } else {
+          printer.text(trimmedLine);
+        }
+      }
+
+      printer.close();
+    });
   } catch (error) {
-    console.error('Error trying to print:', error.message);
+    console.error('Error printing:', error.message);
+  }
+}
+
+async function printBase64Image(base64String, printer) {
+  try {
+    const imagePath = path.join(__dirname, 'temp_image.png');
+
+    const base64Data = base64String.replace(/^data:image\/\w+;base64,/, "");
+    writeFileSync(imagePath, Buffer.from(base64Data, 'base64'));
+
+    console.log(imagePath)
+
+    await new Promise((resolve, reject) => {
+      escpos.Image.load(imagePath, function (image) {
+        printer.align("ct").raster(image);
+        printer.feed(3);
+        writeFileSync(imagePath, '');
+        resolve();
+      });
+    });
+  } catch (error) {
+    console.error('Error printing image:', error.message);
   }
 }
 
--- src/websocket.js
+++ src/websocket.js
@@ -5,7 +5,28 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', (ws) => {
   console.log('WebSocket server connected.');
-  printText('\n\nConnection with printer successfully...\n\n');
+
+   ws.on('message', (message) => {
+    try {
+      const data = JSON.parse(message);
+      if (data.text) {
+        printText(`[center]Hello World![/center]
+[left]This is left-aligned.[/left]
+[LINE]
+[bold]Bold Text[/bold]
+[big]Big Text Here[/big]
+[qr]https://example.com[/qr]
+[img]BASE64 ENCODE[/img]
+[CUT]`, "RONGTA 80mm Series Printer");
+        ws.send(JSON.stringify({ status: 'success', message: 'Impresión completada.' }));
+      } else {
+        ws.send(JSON.stringify({ status: 'error', message: 'Datos inválidos.' }));
+      }
+    } catch (error) {
+      console.error('Error procesando el mensaje:', error.message);
+      ws.send(JSON.stringify({ status: 'error', message: 'Error interno del servidor.' }));
+    }
+  });
 
   ws.on('close', () => {
     console.log('WebSocket server disconnected.');
